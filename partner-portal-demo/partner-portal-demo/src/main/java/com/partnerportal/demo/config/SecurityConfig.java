da9066dcb1eac17f16c7772639d17bb910091e5c
ruben.garruto <ruben.garruto@gmail.com>
2022-09-30 10:04:42 +0200

    implementazione pagina login custom

--- partner-portal-demo/partner-portal-demo/src/main/java/com/partnerportal/demo/config/SecurityConfig.java
+++ partner-portal-demo/partner-portal-demo/src/main/java/com/partnerportal/demo/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.partnerportal.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -36,4 +38,18 @@ public class SecurityConfig
 
 		return new InMemoryUserDetailsManager(gabry, matteo, ruben);
 	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+	{
+
+		return http.authorizeRequests(configurer -> configurer
+				.anyRequest()
+				.authenticated()).formLogin(configurer -> configurer
+						.loginPage("/loginPage")
+						.loginProcessingUrl("/authenticateTheUser")
+						.permitAll())
+				.build();
+	}
+
 }
