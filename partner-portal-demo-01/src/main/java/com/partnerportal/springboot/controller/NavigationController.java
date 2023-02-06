d4d464f05c0a4a13a222d142a3fad4dd89ce2bdf
ruben.garruto <ruben.garruto@rextart.com>
2023-02-06 21:04:11 +0100

    landing page added

--- partner-portal-demo-01/src/main/java/com/partnerportal/springboot/controller/NavigationController.java
+++ partner-portal-demo-01/src/main/java/com/partnerportal/springboot/controller/NavigationController.java
@@ -5,44 +5,38 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class NavigationController {
+public class NavigationController
+{
 
 	// create a mapping for "/hello"
-	
+
 	@GetMapping("/hello")
-	public String sayHello(Model theModel) 
+	public String sayHello(Model theModel)
 	{
-		
+
 		theModel.addAttribute("theDate", new java.util.Date());
-		
+
 		return "helloworld";
 	}
-	
+
 	@GetMapping("/")
-	public String showHome() 
+	public String showHome()
 	{
 		return "home";
 	}
-	
+
 	// add request mapping for /projects
 	@GetMapping("/projects")
-	public String showProjects() 
+	public String showProjects()
 	{
 		return "projects";
 	}
-	
+
 	// add request mapping for /systems
 	@GetMapping("/systems")
-	public String showSystems() 
+	public String showSystems()
 	{
 		return "systems";
 	}
+
 }
-
-
-
-
-
-
-
-
